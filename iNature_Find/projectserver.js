3da1755231d10336f9cd0083e34c881e2bb3b850
freahn <fredrik.ahnlen@gmail.com>
2018-09-25 11:50:17 +0200

    move files
    move files

--- iNature_Find/projectserver.js
+++ iNature_Find/projectserver.js
@@ -5,7 +5,7 @@ const bodyParser = require ('body-parser');
 app.use ( bodyParser.json ()); // for parsing post data that hasjson format
 app.use ( function (req , res , next ) {
   res.setHeader ('Access-Control-Allow-Origin', '*');
-  res.setHeader ('Access-Control-Allow-Methods', 'GET ,POST ,PUT ,DELETE , OPTIONS ');
+  res.setHeader ('Access-Control-Allow-Methods', 'GET ,POST ,PUT ,DELETE ,OPTIONS ');
   res.setHeader ('Access-Control-Allow-Headers', 'Content-Type ');
   next ();
 });
