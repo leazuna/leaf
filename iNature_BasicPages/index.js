b0a5e56438eba62f8802ab661f5fd215c9aa8ce9
leaz <leazuna@gmail.com>
2018-09-26 17:20:09 +0200

    ihopslagning av förstasida och sign in/out

--- iNature_BasicPages/index.js
+++ iNature_BasicPages/index.js
@@ -20,181 +20,291 @@ import DoubleClickZoom from 'ol/interaction/DoubleClickZoom';
 import {LineString, Point} from 'ol/geom.js';
 import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style.js';
 
-
 //------------------- CREATES MAP AND LAYERS -------------------
-//Creates a vector source
-var markerSource = new VectorSource();
-//Defiens marke style
-var markerStyle = new Style({
-  image: new ol.style.Circle({
-    radius: 6,
-    stroke: new ol.style.Stroke({
-      color: 'white',
-      width: 2
-    }),
-    fill: new ol.style.Fill({
-        color:'rgba(32,178,170,0.65)'
-    })
-  })
-});
+
 //Creates a base map - open street map
 var layer = new TileLayer({
   source: new OSM()
 });
-//Creates a vector layer
-var vector = new VectorLayer({
-  source: markerSource,
-  style: markerStyle
-});
+
 //Definas a map
 var map = new Map({
   // controls: ol.control.defaults().extend([new ol.control.FullScreen()]),
   // interactions: ol.interaction.defaults({DoubleClickZoom :false}),
   //target : document.getElementById('map'),
   target : 'map',
-  layers: [layer, vector],
+  layers: [layer],
   view: new View({
     center: fromLonLat([18.476403,57.530952]),
     zoom: 9
   }),
 });
 
-//------------------- LOADS ALL FEATURES FROM DATABASE WHEN REFRESHING PAGE -------------------
-function loadAll(event) {
-  console.log('from test'); //klienten
-  $.ajax({
-    url:'http://localhost:3000/coord',
-    type: 'GET',
-    success: function(res){
-      // console.log(res);
-      // var ajaxDisplay = document.getElementById(map);
-      //                   ajaxDisplay.innerHTML = html
-        for (var i in res) {
-          console.log(res[i].lon);
-          addMarker(res[i].lon, res[i].lat);
-        }
-    }
-  });
-}
 
-//Loads all data form database
-loadAll();
+ //------------------- SIGN IN / SIGN OUT -------------------
 
-//------------------- CREATES FEATURES USING LON,LAT -------------------
-//Creates a feature - a point
-function addMarker(lon, lat) {
-  console.log('lon:', lon);
-  console.log('lat:', lat);
-  //var iconFeatures = [];
-  var iconFeature = new Feature({
-  geometry: new Point(transform([lon, lat], 'EPSG:4326','EPSG:3857'))
-  });
-  markerSource.addFeature(iconFeature);
-}
-
-//------------------- CREATS FEATRUE, STORES IN DATABASE AND TAKES DESCRIPTION INPUT -------------------
-//When double clicking: creates a point and popup with inputs
-map.on('dblclick',function(event){
-  var lonLat = toLonLat(event.coordinate);
-  var descr = description();
-  addMarker(lonLat[0], lonLat[1]); //Creates marker in map using lon & lat
-  storeCoord(lonLat[0], lonLat[1], descr); //Stores corodinates in DB (table 'coord')
-  console.log(descr);
-});
-
-//Function calls the POST function -- gets respons from client and stores
-//coordinates in database
-function storeCoord(lon, lat, descr) {
-  console.log('from post');
-  var request = $.ajax ({
-   url: 'http://localhost:3000/coord',
-   type: "POST",
-   cache: false ,
-   contentType: "application/json",
-   data: JSON.stringify ({ //req body
-     lon: lon,
-     lat: lat,
-     descr: descr
-   })
+ //SIGN IN
+ function signin(form) {
+   var uname = document.getElementById('username_in').value;
+   var psw = document.getElementById('password_in').value;
+   console.log(psw)
+   console.log(uname)
+   var request = $.ajax({
+     url:'http://localhost:3000/signin',
+     type: 'POST',
+     cache: false ,
+     contentType: "application/json",
+     data: JSON.stringify ({ //req body
+       uname: uname,
+       psw: psw
+     }),
+     success: function(res){
+       console.log(res.rows[0].case);
+       var answ = res.rows[0].case;
+       if (answ==true) {
+         window.location.href = "./WEBGISProjectP3.html";
+       }
+       else {
+         alert('The username or password is incorrect, please try again.')
+       }
+     }
    });
-}
+ }
 
-//Popup - asks for description
-function description(event){
-  var txt;
-  var descr = prompt("Description:");
-  if (descr == null || descr == ""){
-    txt = "User cancelled the prompt";
-  } else {
-    txt = descr;
-    return txt;
-  }
-}
+ //SIGN UP
+ function signup(form) {
+   var uname = document.getElementById('username_up').value;
+   var psw= document.getElementById('password_up').value;
+   var confPsw = document.getElementById('confpassword_up').value;
+   var randStr = require('randomstring');
+   var yourString = randStr.generate(8);
+   if (confPsw==psw && psw!="" && confPsw!="") {
+     var request = $.ajax ({
+       url: 'http://localhost:3000/signup',
+       type: "POST",
+       cache: false ,
+       contentType: "application/json",
+       data: JSON.stringify ({ //req body
+         uname: uname,
+         psw: psw,
+         id: yourString
+       }),
+       success: function(res){
+         console.log(res)
+         if (res=='error') {
+           alert("The username is already in use, please choose another one!")
+         }
+         else {
+           alert("Welcome to iNature - please sign in!")
+           $("#signUp .close").click()
+         }
+       }
+     });
+   }
+   else {
+     alert('Please, fill in all fields.')
+   }
+ }
 
-//------------------- SHOWS THE NEAREST FEATURE FROM THE SINGLE CLICK POSITION -------------------
-// Code from: https://openlayers.org/en/latest/examples/synthetic-points.html
-var point = null;
-      var line = null;
-      var displaySnap = function(coordinate) {
-        var closestFeature = markerSource.getClosestFeatureToCoordinate(coordinate);
-        if (closestFeature === null) {
-          point = null;
-          line = null;
-        } else {
-          var geometry = closestFeature.getGeometry();
-          var closestPoint = geometry.getClosestPoint(coordinate);
-          if (point === null) {
-            point = new Point(closestPoint);
-          } else {
-            point.setCoordinates(closestPoint);
-          }
-          if (line === null) {
-            line = new LineString([coordinate, closestPoint]);
-          } else {
-            line.setCoordinates([coordinate, closestPoint]);
-          }
-        }
-        map.render();
-      };
+ var a = document.getElementById("upBtn");
+ a.addEventListener("click",signup);
 
-      //Event handler
-      map.on('singleclick', function(evt) {
-        if (evt.clicking) {
-          return;
-        }
-        var coordinate = map.getEventCoordinate(evt.originalEvent);
-        displaySnap(coordinate);
-      });
+ var b = document.getElementById("inBtn");
+ b.addEventListener("click",signin);
 
-      // Displays snap
-      map.on('singleclick', function(evt) {
-        displaySnap(evt.coordinate);
-      });
 
-      // Line style
-      var stroke = new Stroke({
-        color: 'rgb(255,140,0)',
-        width: 2
-      });
-      var style2 = new Style({
-        stroke: stroke,
-        image: new CircleStyle({
-          radius: 10,
-          stroke: stroke
-        })
-      });
 
-      // Draws the geometry of the point and line on the map
-      map.on('postcompose', function(evt) {
-        var vectorContext = evt.vectorContext;
-        vectorContext.setStyle(style2);
-        if (point !== null) {
-          vectorContext.drawGeometry(point);
-        }
-        if (line !== null) {
-          vectorContext.drawGeometry(line);
-        }
-      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+// //------------------- LOADS ALL FEATURES FROM DATABASE WHEN REFRESHING PAGE -------------------
+// function loadAll(event) {
+//   console.log('from test'); //klienten
+//   $.ajax({
+//     url:'http://localhost:3000/coord',
+//     type: 'GET',
+//     success: function(res){
+//       // console.log(res);
+//       // var ajaxDisplay = document.getElementById(map);
+//       //                   ajaxDisplay.innerHTML = html
+//         for (var i in res) {
+//           console.log(res[i].lon);
+//           addMarker(res[i].lon, res[i].lat);
+//         }
+//     }
+//   });
+// }
+//
+// //Loads all data form database
+// loadAll();
+//
+// //------------------- CREATES FEATURES USING LON,LAT -------------------
+// //Creates a feature - a point
+// function addMarker(lon, lat) {
+//   console.log('lon:', lon);
+//   console.log('lat:', lat);
+//   //var iconFeatures = [];
+//   var iconFeature = new Feature({
+//   geometry: new Point(transform([lon, lat], 'EPSG:4326','EPSG:3857'))
+//   });
+//   markerSource.addFeature(iconFeature);
+// }
+//
+// //------------------- CREATS FEATRUE, STORES IN DATABASE AND TAKES DESCRIPTION INPUT -------------------
+// //When double clicking: creates a point and popup with inputs
+// map.on('dblclick',function(event){
+//   var lonLat = toLonLat(event.coordinate);
+//   var descr = description();
+//   addMarker(lonLat[0], lonLat[1]); //Creates marker in map using lon & lat
+//   storeCoord(lonLat[0], lonLat[1], descr); //Stores corodinates in DB (table 'coord')
+//   console.log(descr);
+// });
+//
+// //Function calls the POST function -- gets respons from client and stores
+// //coordinates in database
+// function storeCoord(lon, lat, descr) {
+//   console.log('from post');
+//   var request = $.ajax ({
+//    url: 'http://localhost:3000/coord',
+//    type: "POST",
+//    cache: false ,
+//    contentType: "application/json",
+//    data: JSON.stringify ({ //req body
+//      lon: lon,
+//      lat: lat,
+//      descr: descr
+//    })
+//    });
+// }
+//
+// //Popup - asks for description
+// function description(event){
+//   var txt;
+//   var descr = prompt("Description:");
+//   if (descr == null || descr == ""){
+//     txt = "User cancelled the prompt";
+//   } else {
+//     txt = descr;
+//     return txt;
+//   }
+// }
+//
+// //------------------- SHOWS THE NEAREST FEATURE FROM THE SINGLE CLICK POSITION -------------------
+// // Code from: https://openlayers.org/en/latest/examples/synthetic-points.html
+// var point = null;
+//       var line = null;
+//       var displaySnap = function(coordinate) {
+//         var closestFeature = markerSource.getClosestFeatureToCoordinate(coordinate);
+//         if (closestFeature === null) {
+//           point = null;
+//           line = null;
+//         } else {
+//           var geometry = closestFeature.getGeometry();
+//           var closestPoint = geometry.getClosestPoint(coordinate);
+//           if (point === null) {
+//             point = new Point(closestPoint);
+//           } else {
+//             point.setCoordinates(closestPoint);
+//           }
+//           if (line === null) {
+//             line = new LineString([coordinate, closestPoint]);
+//           } else {
+//             line.setCoordinates([coordinate, closestPoint]);
+//           }
+//         }
+//         map.render();
+//       };
+//
+//       //Event handler
+//       map.on('singleclick', function(evt) {
+//         if (evt.clicking) {
+//           return;
+//         }
+//         var coordinate = map.getEventCoordinate(evt.originalEvent);
+//         displaySnap(coordinate);
+//       });
+//
+//       // Displays snap
+//       map.on('singleclick', function(evt) {
+//         displaySnap(evt.coordinate);
+//       });
+//
+//       // Line style
+//       var stroke = new Stroke({
+//         color: 'rgb(255,140,0)',
+//         width: 2
+//       });
+//       var style2 = new Style({
+//         stroke: stroke,
+//         image: new CircleStyle({
+//           radius: 10,
+//           stroke: stroke
+//         })
+//       });
+//
+//       // Draws the geometry of the point and line on the map
+//       map.on('postcompose', function(evt) {
+//         var vectorContext = evt.vectorContext;
+//         vectorContext.setStyle(style2);
+//         if (point !== null) {
+//           vectorContext.drawGeometry(point);
+//         }
+//         if (line !== null) {
+//           vectorContext.drawGeometry(line);
+//         }
+//       });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //------------------- STRUNT SOM ÄR BRA ATT HA I FALL ATT -------------------
       // map.on('singleclick', function(evt) {
--- iNature_BasicPages/server.js
+++ iNature_BasicPages/server.js
@@ -9,6 +9,7 @@ app.use ( function (req , res , next ) {
   res.setHeader ('Access-Control-Allow-Headers', 'Content-Type ');
   next ();
 });
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Node-postgres: connection to database
 const { Pool } = require ('pg');
@@ -20,29 +21,49 @@ const pool = new Pool ({
   port: 5432 ,
 });
 
-//Gets values from client and inserts to DB table 'coord'
-app.post ('/coord', (req , res) => { //req = from ajax,
-  console.log ( req.body ); //puts data in consloe for debugging/help when error
-   // data you send from your application is available on req.body object , your data processing logic goes here
-  pool.query (`INSERT INTO coord VALUES ('${req.body.lon}', '${req.body.lat}', '${req.body.descr}') RETURNING *`, function (err , dbResponse ) { //Query that says where the data will be stored and what data that should be stored using a SQL-ish query, if there is anything wrong the DB will send an error message and an error will be returned
-    if ( err) console.log ( err); //if something is wrong put the error message in the console
-    res.setHeader ('Access-Control-Allow-Origin', '*'); 
-    res.send (dbResponse); //the response which is sent to the client
-  });
-});
-
-//Gets data from database - reads all data when the application is started
-app.get ('/coord', (req,res) => {
-  pool.query ('select * from coord', (err, dbResponse ) => { //gets all data from the coord-table
-    if ( err) console.log (err); //shows error message in consol if there is an error when reading
-    console.log (dbResponse.rows); // response to the server
+//Takes username and password as input and checks if it in the DB, is yes - returns true, elsewhere false
+app.post ('/signin', (req,res) => {
+  pool.query (`select case
+when '${req.body.uname}' in (select name from usr) and '${req.body.psw}' in (select password from usr) then true
+else false end`, (err, dbResponse ) => {
+    if ( err)  console.log (err);
+    console.log (dbResponse); // respons till servern
     // here dbResponse is available , your data processing logic goes here
-    res.setHeader ('Access-Control-Allow-Origin', '*'); 
-    res.send (dbResponse.rows); //the response which is sent to the client
+    res.setHeader ('Access-Control-Allow-Origin', '*');
+    res.send (dbResponse); //sänder som repons till klienten
   });
  });
 
+//Takase username, password and a random id as an in put and inserts it into DB
+app.post ('/signup', (req , res) => { //req = from ajax,
+    pool.query (`INSERT INTO usr VALUES ('${req.body.uname}', '${req.body.psw}', '${req.body.id}')`, function (err , dbResponse ) {
+    if ( err) {
+    //  console.log(err);
+      res.send (err.name);
+    }
+    else {
+      res.setHeader ('Access-Control-Allow-Origin', '*');
+      res.send (dbResponse);
+    }
+  });
+});
+
 app.listen (3000 , () => console.log('Example app listening on port 3000!'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //
