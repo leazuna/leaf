2efc2a5cea78a8c37b66590c392c33a788aec991
estblom <evos@kth.se>
2018-09-28 12:09:20 +0200

    FRÅN OCH MED DETTA FUNGERAR ALLT
    inlogget är för närvarande utkommenterat, men fungerar om man kommenterar in det igen

--- iNature_BasicPages/index.js
+++ iNature_BasicPages/index.js
@@ -3,8 +3,6 @@
 //Kanske: https://openlayers.org/en/latest/examples/reusable-source.html
 //Bra för framtiden: https://openlayers.org/en/latest/examples/snap.html
 //https://openlayers.org/en/latest/examples/
-
-//------------------- IMPORTS -------------------
 import 'ol/ol.css';
 import {fromLonLat, toLonLat,transform} from 'ol/proj';
 import {Map, View} from 'ol';
@@ -20,11 +18,34 @@ import DoubleClickZoom from 'ol/interaction/DoubleClickZoom';
 import {LineString, Point} from 'ol/geom.js';
 import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style.js';
 
+
 //------------------- CREATES MAP AND LAYERS -------------------
+//Creates a vector source
+var markerSource = new VectorSource();
+
+//Defiens marke style
+var markerStyle = new Style({
+  image: new ol.style.Circle({
+    radius: 6,
+    stroke: new ol.style.Stroke({
+      color: 'white',
+      width: 2
+    }),
+    fill: new ol.style.Fill({
+        color:'rgba(32,178,170,0.65)'
+    })
+  })
+});
 
 //Creates a base map - open street map
 var layer = new TileLayer({
   source: new OSM()
+});
+
+//Creates a vector layer
+var vector = new VectorLayer({
+  source: markerSource,
+  style: markerStyle
 });
 
 //Definas a map
@@ -33,293 +54,212 @@ var map = new Map({
   // interactions: ol.interaction.defaults({DoubleClickZoom :false}),
   //target : document.getElementById('map'),
   target : 'map',
-  layers: [layer],
+  layers: [layer, vector],
   view: new View({
     center: fromLonLat([18.476403,57.530952]),
     zoom: 9
   }),
 });
 
+//------------------- LOADS ALL FEATURES FROM DATABASE WHEN REFRESHING PAGE -------------------
+function loadAll(event) {
+  console.log('from test'); //klienten
+  $.ajax({
+    url:'http://localhost:3000/coord',
+    type: 'GET',
+    success: function(res){
+      // console.log(res);
+      // var ajaxDisplay = document.getElementById(map);
+      //                   ajaxDisplay.innerHTML = html
+        for (var i in res) {
+          console.log(res[i].lon);
+          addMarker(res[i].lon, res[i].lat);
+        }
+    }
+  });
+}
 
- //------------------- SIGN IN / SIGN OUT -------------------
+//Loads all data form database
+loadAll();
 
- //SIGN IN
- function signin(form) {
-   var uname = document.getElementById('username_in').value;
-   var psw = document.getElementById('password_in').value;
-   console.log(psw)
-   console.log(uname)
-   var request = $.ajax({
-     url:'http://localhost:3000/signin',
-     type: 'POST',
-     cache: false ,
-     contentType: "application/json",
-     data: JSON.stringify ({ //req body
-       uname: uname,
-       psw: psw
-     }),
-     success: function(res){
-       console.log(res.rows[0].case);
-       var answ = res.rows[0].case;
-       if (answ==true) {
-         window.location.href = "./WEBGISProjectP3.html";
-       }
-       else {
-         alert('The username or password is incorrect, please try again.')
-       }
-     }
+//------------------- CREATES FEATURES USING LON,LAT -------------------
+//Creates a feature - a point
+function addMarker(lon, lat) {
+  console.log('lon:', lon);
+  console.log('lat:', lat);
+  //var iconFeatures = [];
+  var iconFeature = new Feature({
+  geometry: new Point(transform([lon, lat], 'EPSG:4326','EPSG:3857'))
+  });
+  markerSource.addFeature(iconFeature);
+}
+
+//------------------- CREATS FEATRUE, STORES IN DATABASE AND TAKES DESCRIPTION INPUT -------------------
+//When double clicking: creates a point and popup with inputs
+map.on('dblclick',function(event){
+  var lonLat = toLonLat(event.coordinate);
+  var descr = description();
+  addMarker(lonLat[0], lonLat[1]); //Creates marker in map using lon & lat
+  storeCoord(lonLat[0], lonLat[1], descr); //Stores corodinates in DB (table 'coord')
+  console.log(descr);
+});
+
+//Function calls the POST function -- gets respons from client and stores
+//coordinates in database
+function storeCoord(lon, lat, descr) {
+  console.log('from post');
+  var request = $.ajax ({
+   url: 'http://localhost:3000/coord',
+   type: "POST",
+   cache: false ,
+   contentType: "application/json",
+   data: JSON.stringify ({ //req body
+     lon: lon,
+     lat: lat,
+     descr: descr
+   })
    });
- }
+}
 
- //SIGN UP
- function signup(form) {
-   var uname = document.getElementById('username_up').value;
-   var psw= document.getElementById('password_up').value;
-   var confPsw = document.getElementById('confpassword_up').value;
-   var randStr = require('randomstring');
-   var yourString = randStr.generate(8);
-   if (confPsw==psw && psw!="" && confPsw!="") {
-     var request = $.ajax ({
-       url: 'http://localhost:3000/signup',
-       type: "POST",
-       cache: false ,
-       contentType: "application/json",
-       data: JSON.stringify ({ //req body
-         uname: uname,
-         psw: psw,
-         id: yourString
-       }),
-       success: function(res){
-         console.log(res)
-         if (res=='error') {
-           alert("The username is already in use, please choose another one!")
-         }
-         else {
-           alert("Welcome to iNature - please sign in!")
-           $("#signUp .close").click()
-         }
-       }
-     });
-   }
-   else {
-     alert('Please, fill in all fields.')
-   }
- }
+//Popup - asks for description
+function description(event){
+  var txt;
+  var descr = prompt("Description:");
+  if (descr == null || descr == ""){
+    txt = "User cancelled the prompt";
+  } else {
+    txt = descr;
+    return txt;
+  }
+}
 
- var a = document.getElementById("upBtn");
- a.addEventListener("click",signup);
+//------------------- SHOWS THE NEAREST FEATURE FROM THE SINGLE CLICK POSITION -------------------
+//Click and get the nearest feature
+var point = null;
+      var line = null;
+      var displaySnap = function(coordinate) {
+        var closestFeature = markerSource.getClosestFeatureToCoordinate(coordinate);
+        if (closestFeature === null) {
+          point = null;
+          line = null;
+        } else {
+          var geometry = closestFeature.getGeometry();
+          var closestPoint = geometry.getClosestPoint(coordinate);
+          if (point === null) {
+            point = new Point(closestPoint);
+          } else {
+            point.setCoordinates(closestPoint);
+          }
+          if (line === null) {
+            line = new LineString([coordinate, closestPoint]);
+          } else {
+            line.setCoordinates([coordinate, closestPoint]);
+          }
+        }
+        map.render();
+      };
 
- var b = document.getElementById("inBtn");
- b.addEventListener("click",signin);
+      map.on('singleclick', function(evt) {
+        if (evt.clicking) {
+          return;
+        }
+        var coordinate = map.getEventCoordinate(evt.originalEvent);
+        displaySnap(coordinate);
+      });
+
+      map.on('singleclick', function(evt) {
+        displaySnap(evt.coordinate);
+      });
+
+      var stroke = new Stroke({
+        color: 'rgb(255,140,0)',
+        width: 2
+      });
+      var style2 = new Style({
+        stroke: stroke,
+        image: new CircleStyle({
+          radius: 10,
+          stroke: stroke
+        })
+      });
+
+      map.on('postcompose', function(evt) {
+        var vectorContext = evt.vectorContext;
+        vectorContext.setStyle(style2);
+        if (point !== null) {
+          vectorContext.drawGeometry(point);
+        }
+        if (line !== null) {
+          vectorContext.drawGeometry(line);
+        }
+      });
 
 
 
+// DET ÄR DET HÄR JAG JOBBAR MED BARA ENDAST OCH INGET OVANFÖR -----------------------------------------------------------------      
+var positionArray = new VectorSource();
 
+function addPositionMarker(lon, lat) {
+  console.log('lon:', lon);
+  console.log('lat:', lat);
+  var iconFeature = new Feature({
+  geometry: new Point(transform([lon, lat], 'EPSG:4326','EPSG:3857'))
+  });
+  positionArray.addFeature(iconFeature);
+}
 
+function geoFindMe() {
+  var output = document.getElementById("out");
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+  function success(position) {
+    //positionArray.clear()
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    if (position.coords.accuracy < 200) {
+      addPositionMarker(longitude, latitude); //Creates marker in map using lon & lat
+      output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+      map.setView(new View({
+        center: fromLonLat([longitude,latitude]),
+        zoom: 15
+      }))
+    }
+    else {
+      output.innerHTML = "<p>Accuracy less than 200 m</p>";
+    }
+  }
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
+  navigator.geolocation.getCurrentPosition(success, error);
 
+}
+function closeWatch() {
+  var output = document.getElementById("out");
+  positionArray.clear()
+  output.innerHTML = "Tracking stopped!"
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//
-// //------------------- LOADS ALL FEATURES FROM DATABASE WHEN REFRESHING PAGE -------------------
-// function loadAll(event) {
-//   console.log('from test'); //klienten
-//   $.ajax({
-//     url:'http://localhost:3000/coord',
-//     type: 'GET',
-//     success: function(res){
-//       // console.log(res);
-//       // var ajaxDisplay = document.getElementById(map);
-//       //                   ajaxDisplay.innerHTML = html
-//         for (var i in res) {
-//           console.log(res[i].lon);
-//           addMarker(res[i].lon, res[i].lat);
-//         }
-//     }
-//   });
-// }
-//
-// //Loads all data form database
-// loadAll();
-//
-// //------------------- CREATES FEATURES USING LON,LAT -------------------
-// //Creates a feature - a point
-// function addMarker(lon, lat) {
-//   console.log('lon:', lon);
-//   console.log('lat:', lat);
-//   //var iconFeatures = [];
-//   var iconFeature = new Feature({
-//   geometry: new Point(transform([lon, lat], 'EPSG:4326','EPSG:3857'))
-//   });
-//   markerSource.addFeature(iconFeature);
-// }
-//
-// //------------------- CREATS FEATRUE, STORES IN DATABASE AND TAKES DESCRIPTION INPUT -------------------
-// //When double clicking: creates a point and popup with inputs
-// map.on('dblclick',function(event){
-//   var lonLat = toLonLat(event.coordinate);
-//   var descr = description();
-//   addMarker(lonLat[0], lonLat[1]); //Creates marker in map using lon & lat
-//   storeCoord(lonLat[0], lonLat[1], descr); //Stores corodinates in DB (table 'coord')
-//   console.log(descr);
-// });
-//
-// //Function calls the POST function -- gets respons from client and stores
-// //coordinates in database
-// function storeCoord(lon, lat, descr) {
-//   console.log('from post');
-//   var request = $.ajax ({
-//    url: 'http://localhost:3000/coord',
-//    type: "POST",
-//    cache: false ,
-//    contentType: "application/json",
-//    data: JSON.stringify ({ //req body
-//      lon: lon,
-//      lat: lat,
-//      descr: descr
-//    })
-//    });
-// }
-//
-// //Popup - asks for description
-// function description(event){
-//   var txt;
-//   var descr = prompt("Description:");
-//   if (descr == null || descr == ""){
-//     txt = "User cancelled the prompt";
-//   } else {
-//     txt = descr;
-//     return txt;
-//   }
-// }
-//
-// //------------------- SHOWS THE NEAREST FEATURE FROM THE SINGLE CLICK POSITION -------------------
-// // Code from: https://openlayers.org/en/latest/examples/synthetic-points.html
-// var point = null;
-//       var line = null;
-//       var displaySnap = function(coordinate) {
-//         var closestFeature = markerSource.getClosestFeatureToCoordinate(coordinate);
-//         if (closestFeature === null) {
-//           point = null;
-//           line = null;
-//         } else {
-//           var geometry = closestFeature.getGeometry();
-//           var closestPoint = geometry.getClosestPoint(coordinate);
-//           if (point === null) {
-//             point = new Point(closestPoint);
-//           } else {
-//             point.setCoordinates(closestPoint);
-//           }
-//           if (line === null) {
-//             line = new LineString([coordinate, closestPoint]);
-//           } else {
-//             line.setCoordinates([coordinate, closestPoint]);
-//           }
-//         }
-//         map.render();
-//       };
-//
-//       //Event handler
+var b = document.getElementById("button");
+b.addEventListener("click",geoFindMe);
+var bc = document.getElementById("buttonClose");
+bc.addEventListener("click",closeWatch);
+// ------------------- STRUNT SOM ÄR BRA ATT HA I FALL ATT -------------------
 //       map.on('singleclick', function(evt) {
 //         if (evt.clicking) {
 //           return;
 //         }
-//         var coordinate = map.getEventCoordinate(evt.originalEvent);
-//         displaySnap(coordinate);
-//       });
-//
-//       // Displays snap
-//       map.on('singleclick', function(evt) {
-//         displaySnap(evt.coordinate);
-//       });
-//
-//       // Line style
-//       var stroke = new Stroke({
-//         color: 'rgb(255,140,0)',
-//         width: 2
-//       });
-//       var style2 = new Style({
-//         stroke: stroke,
-//         image: new CircleStyle({
-//           radius: 10,
-//           stroke: stroke
-//         })
-//       });
-//
-//       // Draws the geometry of the point and line on the map
-//       map.on('postcompose', function(evt) {
-//         var vectorContext = evt.vectorContext;
-//         vectorContext.setStyle(style2);
-//         if (point !== null) {
-//           vectorContext.drawGeometry(point);
-//         }
-//         if (line !== null) {
-//           vectorContext.drawGeometry(line);
+//         var pixel = map.getEventPixel(evt.originalEvent);
+//         var hit = map.hasFeatureAtPixel(pixel);
+//         if (hit) {
+//           map.getTarget().style2.cursor = 'pointer';
+//         } else {
+//           map.getTarget().style2.cursor = '';
 //         }
 //       });
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//------------------- STRUNT SOM ÄR BRA ATT HA I FALL ATT -------------------
-      // map.on('singleclick', function(evt) {
-      //   if (evt.clicking) {
-      //     return;
-      //   }
-      //   var pixel = map.getEventPixel(evt.originalEvent);
-      //   var hit = map.hasFeatureAtPixel(pixel);
-      //   if (hit) {
-      //     map.getTarget().style2.cursor = 'pointer';
-      //   } else {
-      //     map.getTarget().style2.cursor = '';
-      //   }
-      // });
-
 // function post(x, y) {
 //   console.log('from post');
 //   var request = $.ajax ({
@@ -333,13 +273,13 @@ var map = new Map({
 //    })
 //    });
 // }
-
-//Allt som har med knappar att göra
+//
+// Allt som har med knappar att göra
 // var b = document.getElementById('btn');
-//var b2 = document.getElementById('btn2');
+// var b2 = document.getElementById('btn2');
 // b.addEventListener('click', test);
-//b2.addEventListener('click', loadAll);
-
+// b2.addEventListener('click', loadAll);
+//
 // // ------ en annan del som fungerade bra, man la till punkter, se länk: https://openlayers.org/en/latest/examples/draw-features.html
 // // var draw; // global so we can remove it later
 // // function addInteraction() {
@@ -364,7 +304,7 @@ var map = new Map({
 // //
 // //
 // // addInteraction();
-
+//
 // // ---------- det från labben som fungerade från början
 //
 // const map = new Map({
@@ -379,4 +319,4 @@ var map = new Map({
 //     zoom: 9
 //   })
 // });
-//
+
